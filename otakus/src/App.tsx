9c2f9b6bf698cc7781e7fdd6048faaaf35694b3c
skid-kid <polarb587@gmail.com>
2024-11-17 00:38:09 +0530

    added landingpage

---
+++ otakus/src/App.tsx
@@ -0,0 +1,15 @@
+
+import { Landingpage } from './components/Landingpage'
+
+
+function App() {
+ 
+
+  return (
+    <div className='bg-black h-screen w-full'>
+      <Landingpage/>
+    </div>
+  )
+}
+
+export default App
