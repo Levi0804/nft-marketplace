63271ffd9f341839b8a9f7c6b81f659e8dad11b9
skid-kid <polarb587@gmail.com>
2024-11-19 00:10:16 +0530

    typo fix

--- frontend/src/components/CreateNFT.tsx
+++ frontend/src/components/CreateNFT.tsx
@@ -1,6 +1,6 @@
 export const CreateNFT = () => {
     return (
-        <div className="m-96 p-96 text-2xl text-bold">
+        <div className="m-40 p-40 text-2xl text-bold">
             Will soon implement the Create NFT feature,NFT Auction, and maybe NFT fractionalization
         </div>
     )
