5c8efd27bbefe25e761851e268656ba7e6003bd6
skid-kid <polarb587@gmail.com>
2024-11-18 17:14:58 +0530

    minor export changes

--- frontend/src/components/NFTmarketplace.tsx
+++ frontend/src/components/NFTmarketplace.tsx
@@ -4,19 +4,6 @@ import { AptosClient } from 'aptos';
 //const MODULE_ADDRESS = "0x0d8e036d15b11ff270283fb9bd6c5c620d98fd6a871b1494546425cc064b1d34";
 //const MODULE_NAME = "pokemon_marketplace";
 
-type PetraWallet = {
-  connect: () => Promise<{ address: string }>;
-  account: () => Promise<{ address: string }>;
-  disconnect: () => Promise<void>;
-  signAndSubmitTransaction: (transaction: any) => Promise<{ hash: string }>;
-};
-
-declare global {
-  interface Window {
-    //petra?: PetraWallet;
-  }
-}
-
 type NFTCardProps = {
   src: string;
   index: number;
